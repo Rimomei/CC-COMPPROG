a7530e21a9904e200163485116b320010b2d4dff
Rimomei <147568792+Rimomei@users.noreply.github.com>
2024-08-19 13:14:48 +0800

    Add files via upload

---
+++ HELLOWWORLD.c
@@ -0,0 +1,11 @@
+// global decleration
+#include <stdio.h>
+
+//local decleration
+int main()
+{
+// function? idk what this one is, decleration or somt?
+   printf("Hello World");
+// return to original state
+   return 0;
+}
---
+++ HelloworldCplusplus.cpp
@@ -0,0 +1,11 @@
+// global decleration
+#include <iostream>
+// local decleration
+int main()
+// main body or code
+{
+    std::cout<<"Hello World";
+// return to original state
+    return 0;
+}    
+ 
